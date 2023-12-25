ea261d4537eff5853c6e534d4d607a21c2780f26
Sneha <104077029+Sneha12c@users.noreply.github.com>
2023-12-25 07:07:17 +0000

    config file done

---
+++ basic/varaible.js
@@ -0,0 +1,13 @@
+
+const userId = 1234
+let useremail = "sneha@gmail.com"
+var userpassword = "135788";
+let userstate = "goa";
+usercity ;
+
+// userId = 23
+
+console.log(userId);
+
+console.table([useremail , userpassword , usercity , userstate]);
+

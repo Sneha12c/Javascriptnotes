1548b69db6bed09b7b264a9ef798e5a948548067
Sneha <104077029+Sneha12c@users.noreply.github.com>
2023-12-30 17:59:51 +0000

    done

---
+++ basic/conversion.js
@@ -0,0 +1,51 @@
+let name = "337889"
+
+let numbe = Number(name)
+// console.log(typeof(numbe)); // Number 
+// console.log(numbe) // NaN(Not a Number) for "kjjk"
+
+let isclicked = 23
+let strisclicked = "";
+let stringisclicked = "hihello"
+let boolisclicked = Boolean(isclicked);
+let boolstriscked = Boolean(strisclicked);
+let boolstringclicked = Boolean(stringisclicked);
+// console.log(boolisclicked); // 1
+// console.log(boolstriscked); // 0
+// console.log(boolstringclicked); // 1
+
+// 1=> true , 0 => false
+// "" => false , "fjje" => true
+
+let somenumber = 33
+let stringfromnumber = String(somenumber);
+
+// console.log(stringfromnumber);
+
+// ***********Operations***********
+
+str1 = "68"
+str2 = "hfjd"
+// console.log(str1 + str2);
+str3 = str1 + str2
+// console.log(str3);
+// console.log(1+2+2);
+// console.log("3" + 4);
+// console.log(4 + "89");
+// console.log(2+3+"9");
+// Prefer to use parenthesis instead of such messy code
+
+// console.log( +true)
+// console.log(true+) // error
+// console.log(+"")
+
+let num1 , num2 , num3
+num1 = num2 = num3 = 6
+
+// postfix and prefix
+let a = 3
+let b = ++a;
+let c = 9 
+let d = c++
+console.table([a , b , c, d])
+
---
+++ basic/datatype.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs not browser
+
+console.log( 9*
+    8) // code readability should be high
+
+    let x = -9;
+    let swap = false;
+    let value = "hijjek";
+    let y = Math.abs(x);
+    y = Math.E;
+    let z = Math.ceil(-5);
+    let m = Math.clz32(5); // no. leading zeros in binary representation
+    let n = Math.fround(46.378989);
+// datatypes
+// int - range 1 to power(53)
+// bigint =
+// string = ""
+// boolean = true/false
+// undefined 
+// null => standalonevalue
+// 
+console.log(y);
+console.log(z);
+console.log(n);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
